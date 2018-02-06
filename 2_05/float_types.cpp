5a69e729a8acb14db1bb93ba9d6de48eeba2d974
Thomas Mulvey <tmulvey1@pride.hofstra.edu>
2018-02-05 19:30:20 -0500

    made 2_06 folder

---
+++ 2_05/float_types.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    cout << "float type for loopz\n\n";
+    for (double d = 0.0; d <= 5.00000010; d += .01)
+        cout << d << endl;
+    //
+}
