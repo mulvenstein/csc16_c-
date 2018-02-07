96727cdd7594a5cd433de72ec6837c6067901a02
tommulvey5567@gmail.com <tmulvey1@pride.hofstra.edu>
2018-02-06 19:53:10 -0500

    final push of 2_06. more ptr manip

--- 2_06/ptrExp.cpp
+++ 2_06/ptrExp.cpp
@@ -17,11 +17,26 @@ int main() {
    //cout << sizeof(float) << endl;  //4 bytes
    //cout << sizeof(double) << endl; //8 bytes
 
-   typedef int realNum;
+   // if you can never init a * to a var, init to 0.
+   //    fixed ptr = 0;
 
-   realNum i = 0;
-   cout << "Address of integer 'i' : " << &i << endl;
+   typedef int fixed;
 
+   fixed i = 1;
+   cout << "\nAddress of integer 'i' : " << &i << endl; //address of i
 
+   fixed *intPtr = &i; // initializes a pointer that points to address of i
+   cout << "intPtr is referencing :  " << intPtr << endl;
+
+   cout << "intPtr -> val of i :\t " << *intPtr << endl;//prints val of i 
+
+   fixed j = 2;
+   cout << "Address of integer 'j' : " << &j << endl; //address of i 
+
+   intPtr--; //moves down one memory location (will print j val)
+   cout << "Value of intPtr-- == j== " << *intPtr << endl;
+   cout << "Address of intPtr -- :   " << intPtr << endl;
+
+   cout << endl;
    return 0;
 }
