811dcd7fb59ac0d89d04ec1bc6022dff56b709f6
tommulvey5567@gmail.com <tmulvey1@pride.hofstra.edu>
2018-02-07 20:01:36 -0500

    breaking memory with pointers

--- 2_07/memExp.cpp
+++ 2_07/memExp.cpp
@@ -4,10 +4,25 @@ using namespace std;
 
 int main () {
    typedef int fixed;
+   const fixed megaByte = 1024 * 1024;
 
-   fixed *iPtr;
+   fixed *iPtr = 0;
+   cout << "address of *iPtr : " << iPtr << endl;
+
+   const fixed AllocSize = 1000;
+   long byteCount = 0;
+   while(1){
+      iPtr = new int[AllocSize];
+      byteCount += (AllocSize * sizeof(int));
+      cout << (float)(byteCount/megaByte) << endl;
+   }
+
+   /*
    cout << "address of *iPtr : " << iPtr << endl;  
-   cout << *iPtr << endl; //segmentation fault (core dumped) 
+   //cout << *iPtr << endl; //segmentation fault (core dumped) 
+   iPtr = (int *)3000;
+   *iPtr = 3;
+   */
 
    return 0;
 }
