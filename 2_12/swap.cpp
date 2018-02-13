4ae7d3aa216ab64c200a7f9e8d9672965d4c8fed
tommulvey5567@gmail.com <tmulvey1@pride.hofstra.edu>
2018-02-12 19:32:10 -0500

    more referencing:

--- 2_12/swap.cpp
+++ 2_12/swap.cpp
@@ -24,6 +24,10 @@ void swap_by_ref( int &a, int &b ) {
    b = temp;
 } 
 
+void map_add ( int &i ){
+   i = i + 10;
+}
+
 int main () {
    int a = 1;
    int b = 2;
@@ -36,6 +40,19 @@ int main () {
 
    swap_by_ref( a, b );
    cout << "After s-b-r, a: "<< a << " | b: " << b << endl;
+  
+   int arr[] = {0,1,2,3,4,5,6,7,8};
+   cout << "\n\n before : ";
+   for (int idx = 0; idx < 9; ++idx){
+      cout << arr[idx] << " | " ;
+   }   
+   cout << endl;
+   cout << " after : ";
+   for (int idx = 0; idx < 9; ++idx){
+      map_add(arr[idx]);
+      cout << arr[idx] << " | " ;
+   }
+   cout << endl << endl;
    return 0;
 }
 
