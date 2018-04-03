3464dbba1798feadc3639653ea8cfbad2d45448d
tommulvey5567@gmail.com <tmulvey1@pride.hofstra.edu>
2018-04-03 18:40:35 -0400

    nothing

--- 3_26/List.h
+++ 3_26/List.h
@@ -1,13 +1,16 @@
 #ifndef LIST_H
 #define LIST_H
+
 #include "Node.h"
+
 class List
 {
   public:
+    //constructor
     List();
-    void insert(DataType data);
-    void display();
+    void insert(DataType data);//insert data func
+    void display(); //print L.L
   private:
     Node *first;
 };
-#endif //LIST_H
+#endif
